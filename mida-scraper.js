881e44bc217b05a82a4cea00051bf995917394c1
Alessio Cimarelli <jenkin@dataninja.it>
2014-05-21 17:46:10 +0200

    Eureka! setTimeout independent version...

--- mida-scraper.js
+++ mida-scraper.js
@@ -35,48 +35,72 @@ var news = {
   }
 };
 
+//console.log("Apro la prima pagina...");
 var p = window.open(url);
 
-f1();
-
-var f11() {
-  if(!p.window) {
-    setTimeout(f11,t);
-  } else {
+var f11 = function() {
+  //console.log("###f11");
+  if (p.document.readyState === "complete") {
+    //console.log("Memorizzo il titolo...");
     news.title.html += p.document.body.innerHTML;
     f2();
+  } else {
+    //console.log("Aspetto...");
+    setTimeout(f11,t);
   }
 }
 
 var f1 = function() {
-  if (!p.window) {
-    setTimeout(f1,t);
-  } else {
+  //console.log("###f1");
+  if (p.hasOwnProperty("viewPage") && p.document.readyState === "complete") {
     if (n1 < n1Max) {
+      //console.log("Leggo l'elenco...");
       p.viewPage(n1);
       f11();
     } else {
+      //console.log("Metto tutto insieme...");
       news.title.html = "<html><body>" + news.title.html + "</body></html>";
       news.body.html = "<html><body>" + news.body.html + "</body></html>";
       console.log("Finito! Digitare console.log(news.title.html) per la pagina html con l'elenco completo delle news scaricate e console.log(news.body.html) per la pagina con la lista vera e propria delle news. Su Chromium sotto Linux è possibile usare la funzione copy(news.title.html) e copy(news.body.html).");
     }
     n1++;
+  } else {
+    //console.log("Aspetto");
+    setTimeout(f1,t);
+  }
+}
+
+var f21 = function() {
+  //console.log("###f21");
+  if (p.hasOwnProperty("gotogrid") && p.document.readyState === "complete") {
+    //console.log("Memorizzo il testo...");
+    news.body.html += p.document.body.innerHTML;
+    //console.log("Torno all'elenco...");
+    p.gotogrid();
+    f2();
+  } else {
+    //console.log("Aspetto");
+    setTimeout(f21,t);
   }
 }
 
 var f2 = function() {
-  if (n2 < n2Max) {
-    p.viewNews(n2);
-    setTimeout(function() { // Idem
-      news = news + p.document.body.innerHTML;
-      p.gotogrid();
-      setTimeout(f2, t);
-    }, t);
+  //console.log("###f2");
+  if (p.hasOwnProperty("viewNews") && p.document.readyState === "complete") {
+    if (n2 < n2Max) {
+      //console.log("Leggo il testo...");
+      p.viewNews(n2);
+      f21();
+    } else {
+      n2 = 0;
+      f1();
+    }
+    n2++;
   } else {
-    n2 = 0;
-    f1();
+    //console.log("Aspetto");
+    setTimeout(f2,t);
   }
-  n2++;
 }
 
-setTimeout(f1, t);
+f1();
+
